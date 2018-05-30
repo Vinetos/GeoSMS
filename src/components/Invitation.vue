3403675d4c82218ef5eabc525374d7ae35c1e66e
Valentin Chassignol <Vinetosdev@gmail.com>
2018-05-30 15:59:01 +0200

    Update Invitation vue

--- src/components/Invitation.vue
+++ src/components/Invitation.vue
@@ -4,19 +4,39 @@ In this page, we will get the user position and send it to the database (to upda
 */
 <template>
   <div>
-    <p>Your path : {{path}}</p>
-    <p>Lat : {{lat}}</p>
-    <p>Lng : {{lng}}</p>
+    <div id="google-map">
+      <google-map />
+    </div>
   </div>
 </template>
 
+<style scoped>
+  #google-map {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    z-index: -1;
+  }
+</style>
+
 <script>
+import GoogleMap from './GoogleMap'
+import {gmapApi} from 'vue2-google-maps'
+
 export default {
   props: ['path'],
+  components: {
+    GoogleMap
+  },
+  computed: {
+    google: gmapApi
+  },
   data () {
     return {
       lat: undefined,
-      lng: undefined
+      lng: undefined,
+      lastPatientLocationMarker: undefined,
+      location: undefined
     }
   },
   mounted () {
@@ -31,7 +51,8 @@ export default {
           this.lat = position.coords.latitude
           this.lng = position.coords.longitude
           this.sendLocation()
-        })
+          this.updateMap()
+        }, undefined, {enableHighAccuracy: true})
       }
     },
     sendLocation () {
@@ -40,6 +61,25 @@ export default {
           lat: this.lat, lng: this.lng
         })
       }
+    },
+    updateMap () {
+      if (this.lastPatientLocationMarker) {
+        this.lastPatientLocationMarker.setMap(null)
+      }
+      const map = this.$store.state.theGoogleMap
+      // Show a label (and maybe the closest road name ?)
+      const marker = new this.google.maps.Marker({
+        position: {lat: this.lat, lng: this.lng},
+        map: map,
+        title: 'Your location',
+        label: 'P',
+        animation: !this.lastPatientLocationMarker ? this.google.maps.Animation.DROP : undefined
+      })
+      if (!this.lastPatientLocationMarker) {
+        map.setZoom(17)
+        map.setCenter(marker.position)
+      }
+      this.lastPatientLocationMarker = marker
     }
   }
 }
